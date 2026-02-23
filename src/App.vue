908c8e57c2fc3171f4c3296acc7fcd0901b0d1b9
Alain Boudard <alain.boudard@soprasteria.com>
2026-02-23 18:14:35 +0100

    boutons

--- src/App.vue
+++ src/App.vue
@@ -75,12 +75,12 @@
         <template #body="{ data, field, editorInitCallback }">
           <Button
             @click="onEditMode(editorInitCallback)"
-            label="edit"
+            icon="pi pi-pencil" severity="secondary" rounded
             size="small"
           />
           <Button
             @click="onDeleteMode(data, field)"
-            label="delete"
+            icon="pi pi-trash" severity="danger" rounded
             size="small"
           />
         </template>
@@ -94,13 +94,13 @@
               data.price === ''
             "
             severity="secondary"
-            label="save"
+            icon="pi pi-check" rounded
             size="small"
           />
           <Button
             @click="onCancelMode(editorCancelCallback, data)"
             severity="secondary"
-            label="cancel"
+            icon="pi pi-times" rounded
             size="small"
           />
         </template>
